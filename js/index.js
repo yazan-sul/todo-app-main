ad2323e86e740d9d317cddb76932eb71e908abc8
Yazan <yazan5sulaiman@gmail.com>
2025-04-26 19:41:04 +0300

    adding new todo to list

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function addTodo(event){
+    event.preventDefault();
+    const input = document.getElementById('newTodo');
+    const todoText = input.value.trim();
+
+    if(todoText !== ''){
+        const ul = document.getElementById('todo-list');
+        const li = document.createElement('li');
+        li.textContent = todoText;
+        ul.appendChild(li);
+        input.value = '';
+    }
+}
